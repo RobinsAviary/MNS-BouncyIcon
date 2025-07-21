d522cbd91462bb26bb702a1ba409bf1e2f139e3d
RobinsAviary <robinsaviary@gmail.com>
2025-07-21 09:14:32 -0400

    init

---
+++ main.lua
@@ -0,0 +1,45 @@
+pos = Vec2.Zero
+spd = 100;
+dir = Vec2.New(1,1)
+texSize = Vec2.Zero
+
+function Init()
+	Texture.Load("logo", "icon.png")
+	texSize = Texture.Size("logo")
+	SetAntialiasingLevel(16)
+	
+end
+
+function Update()
+	
+	Draw.Clear(Color.Black)
+
+	pos = pos + (dir * Vec2.NewS(spd) * Vec2.NewS(Delta()))
+
+	local winSize = Window.Size()
+
+	local lowerBound = winSize.y - texSize.y
+	local rightBound = winSize.x - texSize.x
+
+	if pos.x < 0 then
+		pos.x = 0
+		dir.x = dir.x * -1
+	end
+
+	if pos.y < 0 then
+		pos.y = 0
+		dir.y = dir.y * -1
+	end
+
+	if pos.x > rightBound then
+		pos.x = rightBound
+		dir.x = dir.x * -1
+	end
+
+	if pos.y > lowerBound then
+		pos.y = lowerBound
+		dir.y = dir.y * -1
+	end
+
+	Draw.Texture("logo", pos)
+end
